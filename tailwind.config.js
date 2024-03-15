810a47ab0ac938c565e8e960378526d4b00810ba
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-03-15 12:54:28 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto-Regular':'Roboto-Regular',
+        'Roboto-Bold':'Roboto-Bold',
+        'Caveat-Regular':'Caveat-Regular',
+        'Spectral':'Spectral',
+      },
+    },
+
+  },
+  plugins: [],
+}
